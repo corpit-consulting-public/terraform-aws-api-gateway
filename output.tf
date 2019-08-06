ba4d3a1e99e5ad34a7955a777c6ea28a3f2162df
Circular-test <nmartinez@corpit-consulting.com>
2019-08-06 10:22:58 -0300

    add new output name

--- output.tf
+++ output.tf
@@ -3,6 +3,11 @@ output "id" {
   value       = "${aws_api_gateway_rest_api.gateway.id}"
 }
 
+output "name" {
+  description = "The id of the apigatewai rest api"
+  value       = "${aws_api_gateway_rest_api.gateway.name}"
+}
+
 output "root_resource_id" {
   value       = "${aws_api_gateway_rest_api.gateway.root_resource_id}"
 }
