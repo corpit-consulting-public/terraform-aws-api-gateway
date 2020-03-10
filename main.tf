98dc44bb8b6d1d17c1f75b0844985ada6cdeaccd
Nicolas Martinez <nmartinez@corpit-consulting.com>
2020-03-10 14:14:06 -0300

    add depends_on

--- main.tf
+++ main.tf
@@ -11,4 +11,5 @@ resource "aws_api_gateway_rest_api" "gateway" {
 resource "aws_api_gateway_deployment" "Deployment" {
   rest_api_id = "${aws_api_gateway_rest_api.gateway.id}"
   stage_name  = "${var.stage_name}"
+  depends_on = ["aws_api_gateway_rest_api.gateway"]
 }
