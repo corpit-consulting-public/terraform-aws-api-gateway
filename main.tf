ae412a131cff44e98996ae50bacb30901c93c575
Nicolas Martinez <nmartinez@corpit-consulting.com>
2020-03-11 13:57:29 -0300

    timestamp

--- main.tf
+++ main.tf
@@ -14,6 +14,7 @@ resource "aws_api_gateway_deployment" "deployment" {
   rest_api_id       = "${aws_api_gateway_rest_api.gateway.id}"
   stage_name        = "${var.stage_name}"
   variables         = "${var.variables}"
+  deployment_at     = "${timestamp()}"
 }
 
 resource "aws_api_gateway_stage" "stage" {
