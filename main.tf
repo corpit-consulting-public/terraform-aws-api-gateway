cd8441285c9c9b2aefa69ce1d4eee984c778d2f1
Nicolas Martinez <nmartinez@corpit-consulting.com>
2020-03-12 14:16:32 -0300

    add new support to api gateway method settings

--- main.tf
+++ main.tf
@@ -25,3 +25,22 @@ resource "aws_api_gateway_stage" "stage" {
   cache_cluster_enabled = "${var.cache_cluster_enabled}"
   xray_tracing_enabled  = "${var.xray_tracing_enabled}"
 }
+
+resource "aws_api_gateway_method_settings" "setting" {
+  rest_api_id = "${aws_api_gateway_rest_api.gateway.id}"
+  stage_name  = "${aws_api_gateway_stage.stage.stage_name}"
+  method_path = "${var.method_path}"
+
+  settings    {
+    logging_level                              = "${var.logging_level}"
+    data_trace_enabled                         = "${var.data_trace_enabled}"
+    metrics_enabled                            = "${var.metrics_enabled}" 
+    throttling_burst_limit                     = "${var.throttling_burst_limit}"
+    throttling_rate_limit                      = "${var.throttling_rate_limit}"
+    caching_enabled                            = "${var.caching_enabled}"
+    cache_ttl_in_seconds                       = "${var.cache_ttl_in_seconds}"
+    cache_data_encrypted                       = "${var.cache_data_encrypted}"
+    require_authorization_for_cache_control    = "${var.require_authorization_for_cache_control}"
+    unauthorized_cache_control_header_strategy = "${var.unauthorized_cache_control_header_strategy}"
+  }
+}
--- variables.tf
+++ variables.tf
@@ -169,3 +169,77 @@ variable "rest_api_id" {
   default = ""
 }
 
+#####Variables for Method Settings
+
+variable "method_path" {
+  type        = "string"
+  description = "Method path defined as {resource_path}/{http_method} for an individual method override, or */* for overriding all methods in the stage."
+}
+
+variable "settings" {
+  type        = "map"
+  description = "The settings block, see below."
+  default     = {}
+}
+
+## Settings
+
+variable "metrics_enabled" {
+  type        = "string"
+  description = " Specifies whether Amazon CloudWatch metrics are enabled for this method. (treu or false)"
+  default     = ""
+}
+
+variable "logging_level" {
+  type        = "string"
+  description = "Specifies the logging level for this method, which effects the log entries pushed to Amazon CloudWatch Logs. The available levels are OFF, ERROR, and INFO."
+  default     = ""
+}
+
+variable "data_trace_enabled" {
+  type        = "string"
+  description = "Specifies whether data trace logging is enabled for this method, which effects the log entries pushed to Amazon CloudWatch Logs."
+  default     = ""
+}
+
+variable "throttling_burst_limit" {
+  type        = "string"
+  description = "Specifies the throttling burst limit."
+  default     = "500"
+}
+
+variable "throttling_rate_limit" {
+  type        = "string"
+  description = "Specifies the throttling rate limit."
+  default     = "10000.0"
+}
+
+variable "caching_enabled" {
+  type        = "string"
+  description = "pecifies whether responses should be cached and returned for requests. A cache cluster must be enabled on the stage for responses to be cached."
+  default     = ""
+}
+
+variable "cache_ttl_in_seconds" {
+  type        = "string"
+  description = "Specifies the time to live (TTL), in seconds, for cached responses. The higher the TTL, the longer the response will be cached."
+  default     = "300"
+}
+
+variable "cache_data_encrypted" {
+  type        = "string"
+  description = "Specifies whether the cached responses are encrypted."
+  default     = ""
+}
+
+variable "require_authorization_for_cache_control" {
+  type        = "string"
+  description = "Specifies whether authorization is required for a cache invalidation request."
+  default     = ""
+}
+
+variable "unauthorized_cache_control_header_strategy" {
+  type        = "string"
+  description = "Specifies how to handle unauthorized requests for cache invalidation. The available values are FAIL_WITH_403, SUCCEED_WITH_RESPONSE_HEADER, SUCCEED_WITHOUT_RESPONSE_HEADER."
+  default     = ""
+}
