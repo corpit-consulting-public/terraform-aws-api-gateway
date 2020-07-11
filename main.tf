e4a61bc71a4893652e162443808a38ac42b96b1a
Nicolas Martinez <nmartinez@corpit-consulting.com>
2020-07-11 17:27:59 -0300

    fix module method settings

--- main.tf
+++ main.tf
@@ -32,17 +32,20 @@ resource "aws_api_gateway_method_settings" "setting" {
   stage_name  = aws_api_gateway_stage.stage.stage_name
   method_path = var.method_path
 
-  settings {
-    logging_level                              = var.logging_level
-    data_trace_enabled                         = var.data_trace_enabled
-    metrics_enabled                            = var.metrics_enabled
-    throttling_burst_limit                     = var.throttling_burst_limit
-    throttling_rate_limit                      = var.throttling_rate_limit
-    caching_enabled                            = var.caching_enabled
-    cache_ttl_in_seconds                       = var.cache_ttl_in_seconds
-    cache_data_encrypted                       = var.cache_data_encrypted
-    require_authorization_for_cache_control    = var.require_authorization_for_cache_control
-    unauthorized_cache_control_header_strategy = var.unauthorized_cache_control_header_strategy
+ dynamic "settings" {
+  for_each = var.settings
+  content {
+    logging_level                              = lookup(settings.value, "logging_level", null)
+    data_trace_enabled                         = lookup(settings.value, "data_trace_enabled", null)
+    metrics_enabled                            = lookup(settings.value, "metrics_enabled", null)
+    throttling_burst_limit                     = lookup(settings.value, "throttling_burst_limit", null)
+    throttling_rate_limit                      = lookup(settings.value, "throttling_rate_limit", null)
+    caching_enabled                            = lookup(settings.value, "caching_enabled", null)
+    cache_ttl_in_seconds                       = lookup(settings.value, "cache_ttl_in_seconds", null)
+    cache_data_encrypted                       = lookup(settings.value, "cache_data_encrypted", null)
+    require_authorization_for_cache_control    = lookup(settings.value, "require_authorization_for_cache_control", null)
+    unauthorized_cache_control_header_strategy = lookup(settings.value, "unauthorized_cache_control_header_strategy", null)
+    }
   }
 }
 
