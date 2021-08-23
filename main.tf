4bb087e90e82fdae0e8f0d7fe8cb068d45758cdc
nicolas-martinnez <nicolas.martinnez@gmail.com>
2021-08-23 17:50:37 -0300

    Update main.tf
    Fix stage_name

--- main.tf
+++ main.tf
@@ -12,7 +12,7 @@ resource "aws_api_gateway_deployment" "deployment" {
   description       = var.deploy_description
   stage_description = var.version_id
   rest_api_id       = aws_api_gateway_rest_api.gateway.id
-  stage_name        = var.stage_name
+  stage_name        = ""
   variables         = var.variables
 }
 
